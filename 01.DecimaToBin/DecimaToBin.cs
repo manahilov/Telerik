f2d246fe768d4a98818e7acc95a7b86f73a3ee73
Boyan Manahilov <boyanmanahilov@yahoo.com>
2013-08-02 15:31:13 +0300

    Decimal to Bin

---
+++ 01.DecimaToBin/DecimaToBin.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Write a program to convert decimal numbers to their binary representation.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class DecimaToBin
+{
+    static void Main()
+    {
+        decimal N;
+        List<byte> binnary = new List<byte>();
+        N = decimal.Parse(Console.ReadLine());
+        do
+        {
+            binnary.Add((byte)(N % 2));
+            N = N / 2;
+        } while (N > 0);
+        binnary.Reverse();
+        foreach (var item in binnary)
+        {
+            Console.Write(item);
+        }
+        Console.ReadLine();
+    }
+}
